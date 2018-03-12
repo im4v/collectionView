38db4334a0164c0b819c0c404ce17d1ac3384717
fuwei <zhangfw@jieshuibao.com>
2018-03-12 13:29:03 +0800

    Initial Commit

---
+++ collectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionView
+//
+//  Created by zhangfuwei on 2018/3/12.
+//  Copyright © 2018年 zhangfuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ collectionView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  collectionView
+//
+//  Created by zhangfuwei on 2018/3/12.
+//  Copyright © 2018年 zhangfuwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
